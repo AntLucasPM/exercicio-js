a8ea2bfd40d9b5825cb88330d7fdad83cc188c0f
Antônio Lucas <135484253+AntLucasPM@users.noreply.github.com>
2025-10-02 14:32:29 +0000

    0.01

---
+++ ex01.js
@@ -0,0 +1,16 @@
+/*
+let produtos = ["Mouse","teclado","Cpu"];
+console.log (produtos[1]);
+
+produtos[2] = "Notebook";
+console.log (produtos[2]);//
+*/
+
+let clientes = ["Lucas", "Antonio", "Marcos"];
+console.log (clientes);
+
+clientes[clientes.length]="Neymar";
+console.log (clientes);
+
+clientes.length = clientes.length - 1;
+console.log (clientes);
