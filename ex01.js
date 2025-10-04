f4097690d521f439d8be9227a840fd70fc7d7909
Antônio Lucas <135484253+AntLucasPM@users.noreply.github.com>
2025-10-04 11:57:08 +0000

    0.02

--- ex01.js
+++ ex01.js
@@ -6,6 +6,7 @@ produtos[2] = "Notebook";
 console.log (produtos[2]);//
 */
 
+/*
 let clientes = ["Lucas", "Antonio", "Marcos"];
 console.log (clientes);
 
@@ -14,3 +15,26 @@ console.log (clientes);
 
 clientes.length = clientes.length - 1;
 console.log (clientes);
+*/
+
+/*
+let produto = {
+    nome: "celular",
+    preco: 1000,
+    quantidade: 10,
+};
+
+produto.preco = 800;
+console.log(produto);
+*/
+
+let venda = {
+    produto: "celular",
+    preco: 1000,
+
+};
+
+venda.data = "01/01/2024";
+delete venda.produto
+
+console.log(venda);
