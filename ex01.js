e2d73b248a6e7f956aa38209df3fc846de7a63da
Antônio Lucas <135484253+AntLucasPM@users.noreply.github.com>
2025-10-07 13:35:41 +0000

    0.03

--- ex01.js
+++ ex01.js
@@ -1,4 +1,4 @@
-/*
+/* Ex01
 let produtos = ["Mouse","teclado","Cpu"];
 console.log (produtos[1]);
 
@@ -6,7 +6,7 @@ produtos[2] = "Notebook";
 console.log (produtos[2]);//
 */
 
-/*
+/* Ex02
 let clientes = ["Lucas", "Antonio", "Marcos"];
 console.log (clientes);
 
@@ -17,7 +17,7 @@ clientes.length = clientes.length - 1;
 console.log (clientes);
 */
 
-/*
+/* Ex03
 let produto = {
     nome: "celular",
     preco: 1000,
@@ -28,6 +28,7 @@ produto.preco = 800;
 console.log(produto);
 */
 
+/* Ex04
 let venda = {
     produto: "celular",
     preco: 1000,
@@ -38,3 +39,23 @@ venda.data = "01/01/2024";
 delete venda.produto
 
 console.log(venda);
+*/
+
+/* Ex05*/
+let estoque = ["Mouse","teclado","Cpu"];
+let novoEstoque = [...estoque];
+novoEstoque[1] = "Monitor";
+
+console.log(estoque);
+console.log(novoEstoque);
+
+let funcionario = {
+    nome: "Lucas",
+    idade: 30,
+};
+
+let novoFuncionario = {...funcionario};
+novoFuncionario.nome = "Antonio";
+
+console.log(funcionario);
+console.log(novoFuncionario);
